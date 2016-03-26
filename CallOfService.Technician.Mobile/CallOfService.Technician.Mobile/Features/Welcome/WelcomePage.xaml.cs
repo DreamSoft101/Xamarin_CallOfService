b544212952d5159b6a5e696339c6a00e9e3d9c6e
Wael Emara <wael.emara@readify.net>
2016-03-27 02:19:03 +1100

    make the welcome page user BasePage

--- CallOfService.Technician.Mobile/CallOfService.Technician.Mobile/Features/Welcome/WelcomePage.xaml.cs
+++ CallOfService.Technician.Mobile/CallOfService.Technician.Mobile/Features/Welcome/WelcomePage.xaml.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CallOfService.Technician.Mobile.UI;
 using Xamarin.Forms;
 
 namespace CallOfService.Technician.Mobile.Features.Welcome
 {
-    public partial class WelcomePage : ContentPage
+    public partial class WelcomePage : BasePage
     {
         public WelcomePage()
         {
             InitializeComponent();
-			NavigationPage.SetHasNavigationBar (this, false);
         }
     }
 }

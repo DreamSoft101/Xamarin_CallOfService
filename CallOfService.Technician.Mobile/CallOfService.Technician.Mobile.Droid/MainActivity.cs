e28bbe2ac88a93d712d49e2754fe7eb253781b60
Wael Emara <wael.s.emara@gmail.com>
2016-05-01 10:39:26 +1000

    add empty android project and update all the packages on the forms project and iOS

--- CallOfService.Technician.Mobile/CallOfService.Technician.Mobile.Droid/MainActivity.cs
+++ CallOfService.Technician.Mobile/CallOfService.Technician.Mobile.Droid/MainActivity.cs
@@ -1,28 +1,29 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
+﻿using Android.App;
 using Android.Widget;
 using Android.OS;
 
 namespace CallOfService.Technician.Mobile.Droid
 {
-	[Activity (
-        Label = "Call Of Service" , 
-        Icon = "@drawable/icon", 
-        MainLauncher = true,  
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-    {
-		protected override void OnCreate (Bundle bundle)
+	[Activity (Label = "CallOfService.Technician.Mobile.Droid", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle savedInstanceState)
 		{
-            base.OnCreate(bundle);
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-        }
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
 	}
 }
 

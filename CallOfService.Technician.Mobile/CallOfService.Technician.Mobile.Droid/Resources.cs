e742a49ea500ed5e053957aa7c67ae52e9e4df64
Wael Emara <wael.emara@readify.net>
2016-05-07 07:46:08 +1000

    add a hack so the app can compile with Xlabs included.

---
+++ CallOfService.Technician.Mobile/CallOfService.Technician.Mobile.Droid/Resources.cs
@@ -0,0 +1,222 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CallOfService.Technician.Mobile.Droid
+{
+    public partial class Resource
+    {
+        public partial class Styleable
+        {
+            public const int Theme_actionBarDivider = ActionBar_divider;
+            public const int Theme_actionBarItemBackground = ActionBar_background;
+            public const int Theme_actionBarPopupTheme = ActionBar_background;
+            public const int Theme_actionBarSize = AppCompatTheme_actionBarSize;
+            public const int Theme_actionBarSplitStyle = AppCompatTheme_actionBarSplitStyle;
+            public const int Theme_actionBarStyle = AppCompatTheme_actionBarStyle;
+            public const int Theme_actionBarTabBarStyle = AppCompatTheme_actionBarTabBarStyle;
+            public const int Theme_actionBarTabStyle = AppCompatTheme_actionBarTabStyle;
+            public const int Theme_actionBarTabTextStyle = AppCompatTheme_actionBarTabTextStyle;
+            public const int Theme_actionBarTheme = AppCompatTheme_actionBarTheme;
+            public const int Theme_actionBarWidgetTheme = AppCompatTheme_actionBarWidgetTheme;
+            public const int Theme_actionButtonStyle = AppCompatTheme_actionButtonStyle;
+            public const int Theme_actionDropDownStyle = AppCompatTheme_actionDropDownStyle;
+            public const int Theme_actionMenuTextAppearance = AppCompatTheme_actionMenuTextAppearance;
+            public const int Theme_actionMenuTextColor = AppCompatTheme_actionMenuTextColor;
+            public const int Theme_actionModeBackground = AppCompatTheme_actionModeBackground;
+            public const int Theme_actionModeCloseButtonStyle = AppCompatTheme_actionModeCloseButtonStyle;
+            public const int Theme_actionModeCloseDrawable = AppCompatTheme_actionModeCloseDrawable;
+            public const int Theme_actionModeCopyDrawable = AppCompatTheme_actionModeCopyDrawable;
+            public const int Theme_actionModeCutDrawable = AppCompatTheme_actionModeCutDrawable;
+            public const int Theme_actionModeFindDrawable = AppCompatTheme_actionModeFindDrawable;
+            public const int Theme_actionModePasteDrawable = AppCompatTheme_actionModePasteDrawable;
+            public const int Theme_actionModePopupWindowStyle = AppCompatTheme_actionModePopupWindowStyle;
+            public const int Theme_actionModeSelectAllDrawable = AppCompatTheme_actionModeSelectAllDrawable;
+            public const int Theme_actionModeShareDrawable = AppCompatTheme_actionModeShareDrawable;
+            public const int Theme_actionModeSplitBackground = AppCompatTheme_actionModeSplitBackground;
+            public const int Theme_actionModeStyle = AppCompatTheme_actionModeStyle;
+            public const int Theme_actionModeWebSearchDrawable = AppCompatTheme_actionModeWebSearchDrawable;
+            public const int Theme_actionOverflowButtonStyle = AppCompatTheme_actionOverflowButtonStyle;
+            public const int Theme_actionOverflowMenuStyle = AppCompatTheme_actionOverflowMenuStyle;
+            public const int Theme_activityChooserViewStyle = AppCompatTheme_activityChooserViewStyle;
+            public const int Theme_alertDialogButtonGroupStyle = AppCompatTheme_alertDialogButtonGroupStyle;
+            public const int Theme_alertDialogCenterButtons = AppCompatTheme_alertDialogCenterButtons;
+            public const int Theme_alertDialogStyle = AppCompatTheme_alertDialogStyle;
+            public const int Theme_alertDialogTheme = AppCompatTheme_alertDialogTheme;
+            public const int Theme_android_windowAnimationStyle = AppCompatTheme_android_windowAnimationStyle;
+            public const int Theme_android_windowIsFloating = AppCompatTheme_android_windowIsFloating;
+            public const int Theme_autoCompleteTextViewStyle = AppCompatTheme_autoCompleteTextViewStyle;
+            public const int Theme_borderlessButtonStyle = AppCompatTheme_borderlessButtonStyle;
+            public const int Theme_buttonBarButtonStyle = AppCompatTheme_buttonBarButtonStyle;
+            public const int Theme_buttonBarNegativeButtonStyle = AppCompatTheme_buttonBarNegativeButtonStyle;
+            public const int Theme_buttonBarNeutralButtonStyle = AppCompatTheme_buttonBarNeutralButtonStyle;
+            public const int Theme_buttonBarPositiveButtonStyle = AppCompatTheme_buttonBarPositiveButtonStyle;
+            public const int Theme_buttonBarStyle = AppCompatTheme_buttonBarStyle;
+            public const int Theme_buttonStyle = AppCompatTheme_buttonStyle;
+            public const int Theme_buttonStyleSmall = AppCompatTheme_buttonStyleSmall;
+            public const int Theme_checkboxStyle = AppCompatTheme_checkboxStyle;
+            public const int Theme_checkedTextViewStyle = AppCompatTheme_checkedTextViewStyle;
+            public const int Theme_colorAccent = AppCompatTheme_colorAccent;
+            public const int Theme_colorButtonNormal = AppCompatTheme_colorButtonNormal;
+            public const int Theme_colorControlActivated = AppCompatTheme_colorControlActivated;
+            public const int Theme_colorControlHighlight = AppCompatTheme_colorControlHighlight;
+            public const int Theme_colorControlNormal = AppCompatTheme_colorControlNormal;
+            public const int Theme_colorPrimary = AppCompatTheme_colorPrimary;
+            public const int Theme_colorPrimaryDark = AppCompatTheme_colorPrimaryDark;
+            public const int Theme_colorSwitchThumbNormal = AppCompatTheme_colorSwitchThumbNormal;
+            public const int Theme_controlBackground = AppCompatTheme_controlBackground;
+            public const int Theme_dialogPreferredPadding = AppCompatTheme_dialogPreferredPadding;
+            public const int Theme_dialogTheme = AppCompatTheme_dialogTheme;
+            public const int Theme_dividerHorizontal = AppCompatTheme_dividerHorizontal;
+            public const int Theme_dividerVertical = AppCompatTheme_dividerVertical;
+            public const int Theme_dropDownListViewStyle = AppCompatTheme_dropDownListViewStyle;
+            public const int Theme_dropdownListPreferredItemHeight = AppCompatTheme_dropdownListPreferredItemHeight;
+            public const int Theme_editTextBackground = AppCompatTheme_editTextBackground;
+            public const int Theme_editTextColor = AppCompatTheme_editTextColor;
+            public const int Theme_editTextStyle = AppCompatTheme_editTextStyle;
+            public const int Theme_homeAsUpIndicator = AppCompatTheme_homeAsUpIndicator;
+            public const int Theme_listChoiceBackgroundIndicator = AppCompatTheme_listChoiceBackgroundIndicator;
+            public const int Theme_listDividerAlertDialog = AppCompatTheme_listDividerAlertDialog;
+            public const int Theme_listPopupWindowStyle = AppCompatTheme_listPopupWindowStyle;
+            public const int Theme_listPreferredItemHeight = AppCompatTheme_listPreferredItemHeight;
+            public const int Theme_listPreferredItemHeightLarge = AppCompatTheme_listPreferredItemHeightLarge;
+            public const int Theme_listPreferredItemHeightSmall = AppCompatTheme_listPreferredItemHeightSmall;
+            public const int Theme_listPreferredItemPaddingLeft = AppCompatTheme_listPreferredItemPaddingLeft;
+            public const int Theme_listPreferredItemPaddingRight = AppCompatTheme_listPreferredItemPaddingRight;
+            public const int Theme_panelBackground = AppCompatTheme_panelBackground;
+            public const int Theme_panelMenuListTheme = AppCompatTheme_panelMenuListTheme;
+            public const int Theme_panelMenuListWidth = AppCompatTheme_panelMenuListWidth;
+            public const int Theme_popupMenuStyle = AppCompatTheme_popupMenuStyle;
+            public const int Theme_popupWindowStyle = AppCompatTheme_popupWindowStyle;
+            public const int Theme_radioButtonStyle = AppCompatTheme_radioButtonStyle;
+            public const int Theme_ratingBarStyle = AppCompatTheme_ratingBarStyle;
+            public const int Theme_searchViewStyle = AppCompatTheme_searchViewStyle;
+            public const int Theme_selectableItemBackground = AppCompatTheme_selectableItemBackground;
+
+            public const int Theme_selectableItemBackgroundBorderless =
+                AppCompatTheme_selectableItemBackgroundBorderless;
+
+            public const int Theme_spinnerDropDownItemStyle = AppCompatTheme_spinnerDropDownItemStyle;
+            public const int Theme_spinnerStyle = AppCompatTheme_spinnerStyle;
+            public const int Theme_switchStyle = AppCompatTheme_switchStyle;
+            public const int Theme_textAppearanceLargePopupMenu = AppCompatTheme_textAppearanceLargePopupMenu;
+            public const int Theme_textAppearanceListItem = AppCompatTheme_textAppearanceListItem;
+            public const int Theme_textAppearanceListItemSmall = AppCompatTheme_textAppearanceListItemSmall;
+
+            public const int Theme_textAppearanceSearchResultSubtitle =
+                AppCompatTheme_textAppearanceSearchResultSubtitle;
+
+            public const int Theme_textAppearanceSearchResultTitle = AppCompatTheme_textAppearanceSearchResultTitle;
+            public const int Theme_textAppearanceSmallPopupMenu = AppCompatTheme_textAppearanceSmallPopupMenu;
+            public const int Theme_textColorAlertDialogListItem = AppCompatTheme_textColorAlertDialogListItem;
+            public const int Theme_textColorSearchUrl = AppCompatTheme_textColorSearchUrl;
+            public const int Theme_toolbarNavigationButtonStyle = AppCompatTheme_toolbarNavigationButtonStyle;
+            public const int Theme_toolbarStyle = AppCompatTheme_toolbarStyle;
+            public const int Theme_windowActionBar = AppCompatTheme_windowActionBar;
+            public const int Theme_windowActionBarOverlay = AppCompatTheme_windowActionBarOverlay;
+            public const int Theme_windowActionModeOverlay = AppCompatTheme_windowActionModeOverlay;
+            public const int Theme_windowFixedHeightMajor = AppCompatTheme_windowFixedHeightMajor;
+            public const int Theme_windowFixedHeightMinor = AppCompatTheme_windowFixedHeightMinor;
+            public const int Theme_windowFixedWidthMajor = AppCompatTheme_windowFixedWidthMajor;
+            public const int Theme_windowFixedWidthMinor = AppCompatTheme_windowFixedWidthMinor;
+            public const int Theme_windowMinWidthMajor = AppCompatTheme_windowMinWidthMajor;
+            public const int Theme_windowMinWidthMinor = AppCompatTheme_windowMinWidthMinor;
+            public const int Theme_windowNoTitle = AppCompatTheme_windowNoTitle;
+            public const int FloatingActionButton_android_background = AppBarLayout_android_background;
+
+            public static readonly int[] Theme = new int[]
+            {
+                2130772264,
+                2130772265,
+                2130772266
+            };
+        }
+
+        public partial class Attribute
+        {
+            public const int mediaRouteSettingsDrawable = mediaRouteAudioTrackDrawable;
+        }
+
+        public partial class Color
+        {
+            public const int abc_color_highlight_material = abc_input_method_navigation_guard;
+            public const int design_textinput_error_color = abc_input_method_navigation_guard;
+        }
+
+        public partial class Dimension
+        {
+            public const int design_fab_content_size = design_fab_elevation;
+            public const int design_navigation_padding_top_default = design_navigation_padding_bottom;
+            public const int design_tab_min_width = design_navigation_padding_bottom;
+            public const int dialog_fixed_height_major = design_navigation_padding_bottom;
+            public const int dialog_fixed_height_minor = design_navigation_padding_bottom;
+            public const int dialog_fixed_width_major = design_navigation_padding_bottom;
+            public const int mr_media_route_controller_art_max_height = design_navigation_padding_bottom;
+            public const int dialog_fixed_width_minor = design_navigation_padding_bottom;
+        }
+
+        public partial class Drawable
+        {
+            public const int abc_control_background_material = abc_cab_background_top_material;
+            public const int ic_setting_dark = abc_cab_background_top_material;
+            public const int ic_setting_light = abc_cab_background_top_material;
+            public const int mr_ic_settings_dark = abc_cab_background_top_material;
+            public const int mr_ic_settings_light = abc_cab_background_top_material;
+        }
+
+        public partial class Id
+        {
+            public const int art = always;
+            public const int buttons = always;
+            public const int default_control_frame = always;
+            public const int disconnect = always;
+            public const int media_route_control_frame = always;
+            public const int media_route_list = always;
+            public const int media_route_volume_layout = always;
+            public const int media_route_volume_slider = always;
+            public const int play_pause = always;
+            public const int route_name = always;
+            public const int settings = always;
+            public const int stop = always;
+            public const int subtitle = always;
+            public const int title_bar = always;
+        }
+
+        public partial class Layout
+        {
+            public const int mr_media_route_chooser_dialog = mr_chooser_dialog;
+            public const int mr_media_route_controller_material_dialog_b = mr_controller_material_dialog_b;
+            public const int mr_media_route_list_item = mr_chooser_list_item;            
+        }
+
+        public partial class String
+        {
+            public const int mr_media_route_button_content_description = mr_button_content_description;
+            public const int mr_media_route_chooser_searching = mr_chooser_searching;
+            public const int mr_media_route_chooser_title = mr_chooser_title;
+            public const int mr_media_route_controller_disconnect = mr_controller_disconnect;
+            public const int mr_media_route_controller_no_info_available = mr_controller_no_info_available;
+            public const int mr_media_route_controller_pause = mr_controller_pause;
+            public const int mr_media_route_controller_play = mr_controller_play;
+            public const int mr_media_route_controller_settings_description = mr_controller_play;
+            public const int mr_media_route_controller_stop = mr_controller_stop;
+        }
+
+        public partial class Style
+        {
+            public const int Base_V22_Theme_AppCompat = Base_V21_Theme_AppCompat;
+            public const int Base_V22_Theme_AppCompat_Light = Base_V21_Theme_AppCompat_Light;
+            public const int Base_V23_Theme_AppCompat = Base_V21_Theme_AppCompat;
+            public const int Base_V23_Theme_AppCompat_Light = Base_V21_Theme_AppCompat_Light;
+            public const int RtlOverlay_Widget_AppCompat_ActionButton_Overflow = Widget_AppCompat_ActionButton_Overflow;
+        }
+    }
+}

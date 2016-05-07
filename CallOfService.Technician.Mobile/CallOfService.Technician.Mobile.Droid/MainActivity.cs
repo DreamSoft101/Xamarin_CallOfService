af98f325c02a1ba60c28a434800167cdec6f4e2d
Wael Emara <wael.s.emara@gmail.com>
2016-05-07 23:17:51 +1000

    Add the xamarin forms init code in android

--- CallOfService.Technician.Mobile/CallOfService.Technician.Mobile.Droid/MainActivity.cs
+++ CallOfService.Technician.Mobile/CallOfService.Technician.Mobile.Droid/MainActivity.cs
@@ -2,28 +2,21 @@
 using Android.Widget;
 using Android.OS;
 using XLabs.Forms;
+using CallOfService.Technician.Mobile.Core.DI;
+using CallOfService.Technician.Mobile.Droid.Core.DI;
 
 namespace CallOfService.Technician.Mobile.Droid
 {
 	[Activity (Label = "CallOfService.Technician.Mobile.Droid", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : XFormsApplicationDroid
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
-
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			Xamarin.FormsMaps.Init(this,savedInstanceState);
+			DependencyResolver.Initialize(new AndroidModule(), new FormsModule());
+			LoadApplication(new App());
 		}
 	}
 }
